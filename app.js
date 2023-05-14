46a4d7b1bc0ecf4801595446d3b97bccbae5c929
Yookie Bui <81119866+yookie-bui@users.noreply.github.com>
2023-05-14 22:39:44 +1200

    winning score selection, rewrite event listener function

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const userScore = document.getElementById('player-score');
 const computerScore = document.getElementById('computer-score');
 const gameText = document.getElementById('game-text');
 const weaponList = ['Rock', 'Paper', 'Scissors'];
+const resetButton = document.getElementById('reset-button');
+resetButton.disabled = true;
+
 
 const getComputerWeapon = () => {
     let weapon = ['Rock', 'Paper', 'Scissors'];
@@ -14,14 +17,19 @@ const getComputerWeapon = () => {
     return computerWeapon;
 }
 
+const getUserWeapon = (btn) => {
+    if(resetButton.disabled == true) {
+        user.innerHTML = btn.value;
+        playground(btn.value);
+    }  
+}
+
 const playground = (user) => {
     let computer = getComputerWeapon();
     let user_score = parseInt(userScore.innerHTML);
     let computer_score = parseInt(computerScore.innerHTML);
     let weapons = [user, computer];
-    let scores = [user_score, computer_score];
-    console.log(user);
-    console.log(weapons.indexOf(user));
+    let targetScore = document.getElementById('scores-select').value;
     if (weapons[0] === weapons[1]) {
         gameText.innerHTML = "DRAW!!!"
     } else {
@@ -30,33 +38,61 @@ const playground = (user) => {
         let indexes = [userWeaponIndex, computerWeaponIndex];
         if (indexes.toString == '[1,3]') {
             user_score += 1;
-            gameText.innerHTML = "You Win";
+            gameText.innerHTML = "Hurray!!!";
         } else if (indexes.toString == '[3,1]') {
             computer_score += 1;
-            gameText.innerHTML = "You Lose";
+            gameText.innerHTML = "It's a back luck.";
         } else {
             let maxIndex = Math.max(...indexes);
             let indexOfMax = indexes.indexOf(maxIndex);;
             if (indexOfMax == 0) {
                 user_score += 1;
-                gameText.innerHTML = "You Win";
+                gameText.innerHTML = "Hurray!!!";
             } else {
                 computer_score += 1;
-                gameText.innerHTML = "You Lose";
+                gameText.innerHTML = "It's a back luck";
             }
         }
     }
     userScore.innerHTML = user_score;
     computerScore.innerHTML = computer_score;
-    
-
+    let scores = [user_score, computer_score];
+    if (scores.includes(parseInt(targetScore))) {
+        let indexOfWinner = scores.indexOf(parseInt(targetScore));
+        if (indexOfWinner == 0) {
+            gameText.innerHTML = "You Win!!!";
+            gameText.style.color = "green";
+            userScore.style.color = "green";
+            userScore.style.borderColor = "green";
+            computerScore.style.color = "red";
+            computerScore.style.borderColor = "red";
+        } else {{
+            gameText.innerHTML = "You Lose!!!";
+            gameText.style.color = "red";
+            userScore.style.color = "red";
+            userScore.style.borderColor = "red";
+            computerScore.style.color = "green";
+            computerScore.style.borderColor = "green";
+        }}
+        resetButton.disabled = false;
+    }
 }
 
 btns.forEach(btn => {
-    btn.addEventListener ('click', () => {
-        // Get user weapon
-        let userWeapon = btn.value;
-        user.innerHTML = userWeapon;
-        playground(userWeapon);
-    })
+    btn.addEventListener ('click',() => {getUserWeapon(btn)}, "")
 });
+
+resetButton.addEventListener('click', () => {
+    userScore.innerHTML = 0;
+    computerScore.innerHTML = 0;
+    gameText.innerHTML = "Get Your Weapon";
+    user.innerHTML = "";
+    computer.innerHTML = "";
+    resetButton.disabled = !resetButton.disabled;
+    gameText.style.color = "black";
+    userScore.style.color = "black";
+    userScore.style.borderColor = "black";
+    computerScore.style.color = "black";
+    computerScore.style.borderColor = "black";
+    resetButton.disabled == true;
+})
